8437e159737e34cc65934405696ac01f03ba58d9
baajayi <delly31@hotmail.com>
2024-02-05 17:12:22 +0100

    working on beautifulsoup

--- web_scrap2.py
+++ web_scrap2.py
@@ -6,21 +6,18 @@ import ssl
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+count = int(input("Enter Count: "))
+position = int(input("Enter Position: "))
 
 url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
+tags=soup("a")
 
 # Retrieve all of the anchor tags
-tags = soup('a')
-known_list = []
-third_page=tags[3].get("href", None)
-for i in range(4):
-    url = third_page
-    html = urllib.request.urlopen(url, context=ctx).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    tags = soup("a")
-    known_list.append(tags[i].contents[0])
+known_list=[]
+for i in range(count):
+    known_list.append(tags[position].contents[0])
 
 
 print(known_list)
